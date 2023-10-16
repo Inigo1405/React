7b7aab2fa8dcc5292e06f08a47ad296a77785de9
Iñigo Quintana <quintanadinigo@gmail.com>
2023-10-16 10:25:33 -0600

    add component Amigos/Amigo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
+let sesion = true;
+const amigos = ['Vinny', 'Pingul', 'Nao', 'Fredos', 'Sabine'];
+
+
+function Header({title, color}){
+  console.log(title)
+  return <h1 style={{color: color}}> {title ? title : "No hay nada"} JSX </h1>
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header title="Hola" />
+        <Header title="Adios" color="red" />
+        <Header color="green"/>
+
       </header>
+
+      {sesion === true ? 
+        <>
+        <Header title="Bienvenidos" />
+        <Amigos />
+        {/* <ul>
+          {amigos.map((amigo, index) => {
+            return <li key={index}>{amigo}</li>
+          })}
+        </ul> */}
+        </>
+        :
+        <p>No has iniciado sesion...</p>
+      }
+
     </div>
   );
 }
+
+
+function Amigos (){
+  return (
+    amigos.map((amigo, index) => {
+      return Amigo(amigo, index)
+    })
+  )
+}
+
+function Amigo (amigo, index){
+  return <p>{index}.- {amigo} </p>
+}
+// Crear el componente 
+// amigos
+// amigo  
 
 export default App;
